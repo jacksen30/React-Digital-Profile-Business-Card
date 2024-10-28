be7db564dfee89c8d5d1029cc7438a1f4445881a
Jacksen Nillson <nillsonjacksen@gmail.com>
2024-10-28 16:34:22 +0800

    import social icons for footer, layout footer as a flexbox, style icons as per figma design

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,15 @@
+import facebookIcon from '../assets/icons/facebook.png';
+import githubIcon from '../assets/icons/github.png';
+import instagramIcon from '../assets/icons/instagram.png';
+import linkedinIcon from '../assets/icons/linkedin.png';
+
 function Footer() {
     return (
         <footer className="footer">
-            <a className="social-icon" href="" alt="logo"><i src="../assets/icons/linkedin.png"></i></a>
-            <a className="social-icon" href="" alt="logo"></a>
-            <a className="social-icon" href="" alt="logo"></a>
-            <a className="social-icon" href="" alt="logo"></a>
+            <a href="" alt="github logo"><img className="social-icon" src={githubIcon} /></a>
+            <a href="" alt="LinkedIn logo"><img className="social-icon" src={linkedinIcon} /></a>
+            <a href="" alt="Instagram logo"><img className="social-icon" src={instagramIcon} /></a>
+            <a href="" alt="Facebook logo"><img className="social-icon" src={facebookIcon} /></a>
         </footer>
     )
 }
